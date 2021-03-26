547a2084ae1472566326fd99857b014d824315c8
cofe <cofedream@outlook.com>
2021-03-26 09:55:43 +0800

    优化实体类字段索引ResultMap的Property

--- core/src/main/java/tk/cofe/plugin/mybatis/search/FieldReferenceSearch.java
+++ core/src/main/java/tk/cofe/plugin/mybatis/search/FieldReferenceSearch.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 cofe
+ * Copyright (C) 2019-2021 cofe
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,34 +19,32 @@ package tk.cofe.plugin.mybatis.search;
 
 import com.intellij.openapi.application.QueryExecutorBase;
 import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.psi.PsiClass;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiField;
+import com.intellij.psi.*;
 import com.intellij.psi.search.searches.ReferencesSearch;
-import com.intellij.psi.xml.XmlAttributeValue;
 import com.intellij.util.Processor;
+import com.intellij.util.xml.GenericAttributeValue;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import tk.cofe.plugin.common.utils.PsiJavaUtils;
 import tk.cofe.plugin.mybatis.dom.model.attirubte.PropertyAttribute;
 import tk.cofe.plugin.mybatis.dom.model.tag.ResultMap;
-import tk.cofe.plugin.mybatis.psi.reference.XmlAttributeValueReference;
 import tk.cofe.plugin.mybatis.service.MapperService;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 /**
  * @author : zhengrf
  * @date : 2019-01-23
  */
-public class FieldReferenceSearch extends QueryExecutorBase<XmlAttributeValueReference, ReferencesSearch.SearchParameters> {
+public class FieldReferenceSearch extends QueryExecutorBase<PsiReference, ReferencesSearch.SearchParameters> {
 
     public FieldReferenceSearch() {
         super(true);
     }
 
     @Override
-    public void processQuery(@NotNull ReferencesSearch.SearchParameters queryParameters, @NotNull Processor<? super XmlAttributeValueReference> consumer) {
+    public void processQuery(@NotNull ReferencesSearch.SearchParameters queryParameters, @NotNull Processor<? super PsiReference> consumer) {
         PsiElement fieldElement = queryParameters.getElementToSearch();
         if (!(fieldElement instanceof PsiField)) {
             return;
@@ -67,24 +65,28 @@ public class FieldReferenceSearch extends QueryExecutorBase<XmlAttributeValueRef
                 .forEach(attributes -> process(attributes, psiField, consumer));
     }
 
-    private void process(List<? extends PropertyAttribute> attributes, PsiField psiField, Processor<? super XmlAttributeValueReference> consumer) {
-        attributes.forEach(id -> {
-            XmlAttributeValue xmlAttributeValue = id.getProperty().getXmlAttributeValue();
-            if (xmlAttributeValue != null && Objects.equals(psiField.getName(), xmlAttributeValue.getValue())) {
-                consumer.process(new XmlAttributeValueReference(xmlAttributeValue));
-            }
-        });
+    private void process(List<? extends PropertyAttribute> attributes, PsiField psiField, Processor<? super PsiReference> consumer) {
+        final String fieldName = psiField.getName();
+        attributes.forEach(id -> Optional.ofNullable(id.getProperty())
+                .map(GenericAttributeValue::getXmlAttributeValue)
+                .filter(val -> Objects.equals(fieldName, val.getValue()))
+                .ifPresent(val -> consumer.process(new PsiReferenceBase<>(val) {
+                    @Override
+                    public @NotNull PsiElement resolve() {
+                        return psiField;
+                    }
+                })));
     }
 
-    private boolean isTarget(@Nullable PsiClass targetClass, @NotNull String currentClassQualifiedName) {
+    private boolean isTarget(PsiClass targetClass, String currentClassQualifiedName) {
         if (targetClass == null) {
+            return false;
+        }
+        if (PsiJavaUtils.isObjectClass(targetClass)) {
             return false;
         }
         if (currentClassQualifiedName.equals(targetClass.getQualifiedName())) {
             return true;
-        }
-        if ("java.lang.Object".equals(targetClass.getQualifiedName())) {
-            return false;
         }
         return isTarget(targetClass.getSuperClass(), currentClassQualifiedName);
     }
