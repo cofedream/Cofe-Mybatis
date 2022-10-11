505308c906c72277dce7cd710cd6fe2158512730
cofe <cofedream@outlook.com>
2022-10-11 23:33:54 +0800

    feat: 依赖升级
    1.gradle升级至7.5.1
    2.gralde intellij plugin升级至1.9.0
    3.添加gradle构建缓存

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,21 @@
 /*
+ * Copyright (C) 2019-2022 cofe
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/*
  * Copyright (C) 2019-2021 cofe
  *
  * This program is free software: you can redistribute it and/or modify
@@ -18,7 +35,7 @@ fun properties(key: String) = project.findProperty(key).toString()
 
 plugins {
     java
-    id("org.jetbrains.intellij") version "1.5.2" apply (false)
+    id("org.jetbrains.intellij") version "1.9.0" apply (false)
 }
 
 group = properties("group")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019-2021 cofe
+ * Copyright (C) 2019-2022 cofe
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,3 +20,4 @@ include("resources_zh")
 include("lang")
 include("core")
 
+enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
